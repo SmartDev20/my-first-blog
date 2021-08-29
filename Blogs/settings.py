fd92a53c4d0a22752606fd06058dd009ddc34df7
Nabih Elshabrawi <nabieh_elshabrawy@outlook.com>
2021-08-29 06:00:58 +0200

    Chamged the HTML for the site.

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Blogs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MyDB',
-        'USER' : 'postgres' ,
-        'PASSWORD' : 'postgres' ,
-        'HOST' : 'localhost' ,
-        'PORT' : '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
